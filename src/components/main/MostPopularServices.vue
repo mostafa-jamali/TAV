1b376a25cf8662bf3db92bd810def2f6580b432e
mostafa jamali <m.jamali20274@gmail.com>
2022-11-23 23:34:24 +0330

    add popular services component

---
+++ src/components/main/MostPopularServices.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="most-popular-services mt-4 border">
+    <div class="d-flex justify-content-between">
+      <p><b>Most Popular Services</b></p>
+      <a href="/"><b class="see-all-link">See All</b></a>
+    </div>
+    <div>
+      <ul class="d-flex list-unstyled">
+        <li class="pl-3" v-for="service in servicesList">
+          <a href="/" class="text-secondary">{{ service }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MostPopularServices',
+  data() {
+    return {
+      servicesList: ['All', 'Cleaning', 'Repairing', 'Painting', 'Laundry', 'Appliance'],
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,17 @@
         <!-- <img src="../assets/images/Capture.jpg" alt=""> -->
         <special-offers></special-offers>
         <services></services>
+        <most-popular-services></most-popular-services>
       </b-col>
     </b-row>
   </b-container>
 </template>
 <script>
+import MostPopularServices from '../components/main/MostPopularServices.vue';
 import Services from '../components/main/Services/Services.vue';
 import SpecialOffers from '../components/main/SpecialOffers.vue';
 export default {
-  components: { SpecialOffers, Services },
+  components: { SpecialOffers, Services, MostPopularServices },
   name: 'Home',
 };
 </script>
